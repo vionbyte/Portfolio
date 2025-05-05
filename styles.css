e1226cdb2453e106ab3b28f591bd16bb95c7b1bc
ApsXminer <inkmcontop@gmail.com>
2025-05-05 13:56:37 +0530

    styles.css

---
+++ styles.css
@@ -0,0 +1,22 @@
+        ::-webkit-scrollbar {
+            width: 6px;
+            height: 6px;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background-color: rgba(156, 163, 175);
+            border-radius: 3px;
+        }
+
+        ::-webkit-scrollbar-track {
+            background-color: transparent;
+            content: "";
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background-image: inherit;
+            background-size: cover;
+            z-index: 0;
+        }
